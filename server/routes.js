88f2eacf1c30f1aee1439a9b0306bba4d8cb6038
Samyog Ghimire <43634327+sambo2299@users.noreply.github.com>
2019-01-23 17:58:15 +0545

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -12,6 +12,12 @@ const controller = require('./controller');
 
 module.exports = (app) => {
 	
+	app.use(function(req, res, next) {
+  		res.header("Access-Control-Allow-Origin", "*");
+  		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	next();
+	});
+	
 	// app.use(tracer);
 	app.get('/', (req, res) => {
 		res.render('index.html');
