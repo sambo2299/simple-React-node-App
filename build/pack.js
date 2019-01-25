cfb99295ed586face19c547dc20f7b8af6665c1e
Samyog Ghimire <43634327+sambo2299@users.noreply.github.com>
2019-01-25 15:47:19 +0545

    Update pack.js

--- build/pack.js
+++ build/pack.js
@@ -5,6 +5,7 @@ octo.pack()
   .append('./public/**')
   .append('./lib/**')
   .append('./client/**')
+  .append('./node_modules/**')
   .toFile('./bin/', function (err, data) {
     console.log("Package Saved: "+ data.name);
   });
