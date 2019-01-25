6caf56007a294d203975b7481e21bcc525d9296f
samyog <samyog.ghimire@javra.com>
2019-01-25 14:44:43 +0545

    build pack changed

--- build/pack.js
+++ build/pack.js
@@ -1,10 +1,11 @@
 var octo = require('@octopusdeploy/octopackjs');
 octo.pack()    
   .append('./package.json')
-  .append('./server/*')
-  .append('./public/*')
-  .append('./lib/*')
-  .append('./client/*')
-  .toFile('./bin', function (err, data) {
+  .append('./server/**')
+  .append('./public/**')
+  .append('./lib/**')
+  .append('./client/**')
+  .toFile('./bin/', function (err, data) {
     console.log("Package Saved: "+ data.name);
   });
+
