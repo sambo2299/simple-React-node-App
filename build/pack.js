a8e5354ad099d52884d5d88de7c5fff3519d633e
Samyog Ghimire <43634327+sambo2299@users.noreply.github.com>
2019-01-28 17:51:56 +0545

    Update pack.js

--- build/pack.js
+++ build/pack.js
@@ -1,12 +1,12 @@
 var octo = require('@octopusdeploy/octopackjs');
-console.log(`${%build.counter%}`);
-// octo.pack('targz', { version: process.argv})    
-//   .append('./package.json')
-//   .append('./server/**')
-//   .append('./public/**')
-//   .append('./lib/**')
-//   .append('./client/**')  
-//   .toFile('./bin/', function (err, data) {
-//     console.log("Package Saved: "+ data.name);
-//   });
+
+octo.pack('targz', { version: process.argv[process.argv.length - 1]})    
+  .append('./package.json')
+  .append('./server/**')
+  .append('./public/**')
+  .append('./lib/**')
+  .append('./client/**')  
+  .toFile('./bin/', function (err, data) {
+    console.log("Package Saved: "+ data.name);
+  });
 
