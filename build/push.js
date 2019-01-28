a49f3366dd2758cfeef6c9ce08b2cde56a34ba9f
Samyog Ghimire <43634327+sambo2299@users.noreply.github.com>
2019-01-28 17:53:33 +0545

    Update push.js

--- build/push.js
+++ build/push.js
@@ -1,6 +1,6 @@
 var octo = require('@octopusdeploy/octopackjs');
 
-octo.push('./bin/simple-Mern.1.0.0.tar.gz', {
+octo.push(`./bin/simple-Mern.${process.argv[process.argv.length -1 ]}.tar.gz`, {
         host: 'http://10.0.1.102:9999/', 
         apikey: 'API-54KQSIPUQABY8WZS4TCKU6Q',
         replace: true
